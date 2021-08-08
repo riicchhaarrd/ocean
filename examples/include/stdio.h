0a61da242e54687aebf7235a47dba11889b79c04
Richard <riicchhaarrd@users.noreply.github.com>
2021-08-09 01:07:00 +0200

    print_hex and print_decimal fix

--- examples/include/stdio.h
+++ examples/include/stdio.h
@@ -21,18 +21,36 @@ void putchar(int ch)
     print(buf);
 }
 
-void print_decimal(int d)
+void print_hex(int d)
 {
     char buf[32];
     int i = 0;
     buf[31] = 0;
     while(d > 0)
 	{
+        int m = d % 16;
+        if(m < 10)
+        buf[sizeof(buf) - i - 2] = m + '0';
+        if(m >= 10)
+            buf[sizeof(buf) - i - 2] = (m-10) + 'A';
+        d /= 16;
+        i += 1;
+	}
+    print(&buf[sizeof(buf) - i - 1]);
+}
+
+void print_decimal(int d)
+{
+    char buf[32];
+    int i = 0;
+    buf[31] = 0;
+    do
+	{
         int m = d % 10;
         buf[sizeof(buf) - i - 2] = m + '0';
         d /= 10;
         i += 1;
-	}
+	} while(d > 0);
     print(&buf[sizeof(buf) - i - 1]);
 }
 
