bbc7162b6d951b05e963f9a770bee69dbaa5756b
Richard <riicchhaarrd@users.noreply.github.com>
2021-08-12 10:22:14 +0200

    move functions to util

--- elf.c
+++ elf.c
@@ -1,73 +1,11 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <string.h>
 #include <assert.h>
 #include <errno.h>
 
 #include "compile.h"
 #include "rhd/linked_list.h"
-#include "rhd/heap_string.h"
-#include "types.h"
-
-static void dd(heap_string *s, uint32_t i)
-{
-    union
-    {
-	uint32_t i;
-	uint8_t b[4];
-    } u = { .i = i };
-    
-    for(size_t i = 0; i < 4; ++i)
-	heap_string_push(s, u.b[i]);
-}
-
-static void dw(heap_string *s, uint16_t i)
-{
-    union
-    {
-	uint16_t s;
-	uint8_t b[2];
-    } u = { .s = i };
-
-    heap_string_push(s, u.b[0]);
-    heap_string_push(s, u.b[1]);
-}
-
-static void db(heap_string *s, u8 op)
-{
-    heap_string_push(s, op);
-}
-
-static void pad(heap_string *s, u32 n)
-{
-    for(int i = 0; i < n; ++i)
-        heap_string_push(s, i & 0xff);
-}
-
-static int align_to(int pos, int align)
-{
-    if(pos % align == 0)
-        return pos; //no alignment needed
-    return align - (pos % align);
-}
-
-static void pad_align(heap_string *s, int align)
-{
-    int pos = heap_string_size(s);
-    if(pos % align == 0)
-        return; //no alignment needed
-    int m = align - (pos % align);
-    for(int i = 0; i < m; ++i)
-        heap_string_push(s, 0);
-}
-
-static void buf(heap_string *s, const char *buf, size_t len)
-{
-    for(size_t i = 0; i < len; ++i)
-    {
-		heap_string_push(s, buf[i] & 0xff);
-    }
-}
+#include "util.h"
 
 struct __attribute__((__packed__)) phdr32
 {
---
+++ util.h
@@ -0,0 +1,73 @@
+#ifndef UTIL_H
+#define UTIL_H
+#include "rhd/heap_string.h"
+#include <stdint.h>
+#include "types.h"
+
+static int dd(heap_string *s, uint32_t i)
+{
+	int sz = heap_string_size( s );
+	union
+	{
+		uint32_t i;
+		uint8_t b[4];
+	} u = { .i = i };
+
+	for ( size_t i = 0; i < 4; ++i )
+		heap_string_push( s, u.b[i] );
+
+	return sz;
+}
+
+static int dw( heap_string* s, uint16_t i )
+{
+	int sz = heap_string_size( s );
+	union
+	{
+		uint16_t s;
+		uint8_t b[2];
+	} u = { .s = i };
+
+	heap_string_push( s, u.b[0] );
+	heap_string_push( s, u.b[1] );
+	return sz;
+}
+
+static int db(heap_string *s, u8 op)
+{
+    heap_string_push(s, op);
+    return heap_string_size(s) - 1;
+}
+
+static void pad(heap_string *s, u32 n)
+{
+    for(int i = 0; i < n; ++i)
+        heap_string_push(s, 0x0);
+}
+
+static int align_to(int pos, int align)
+{
+    if(pos % align == 0)
+        return pos; //no alignment needed
+    return align - (pos % align);
+}
+
+static void pad_align(heap_string *s, int align)
+{
+    int pos = heap_string_size(s);
+    if(pos % align == 0)
+        return; //no alignment needed
+    int m = align - (pos % align);
+    for(int i = 0; i < m; ++i)
+        heap_string_push(s, 0);
+}
+
+static void buf(heap_string *s, const char *buf, size_t len)
+{
+    for(size_t i = 0; i < len; ++i)
+    {
+		heap_string_push(s, buf[i] & 0xff);
+    }
+}
+
+#endif
