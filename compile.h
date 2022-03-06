776e1687efdec601a4ac42efd296e32c625123f1
Richard <riicchhaarrd@users.noreply.github.com>
2022-03-06 14:18:53 +0100

    x64 registers

--- compile.h
+++ compile.h
@@ -8,18 +8,28 @@
 
 typedef enum
 {
-    EAX,
-    ECX,
-    EDX,
-    EBX,
-    ESP,
-    EBP,
-    ESI,
-    EDI,
-	
-	EIP,
-	REGISTER_X86_FLAGS,
-	REGISTER_X86_MAX
+    EAX, //0
+    ECX, //1
+    EDX, //2
+    EBX, //3
+    ESP, //4
+    EBP, //5
+    ESI, //6
+    EDI, //7
+
+    //x64
+    R8,
+    R9,
+    R10,
+    R11,
+    R12,
+    R13,
+    R14,
+    R15,
+
+    EIP,
+    REGISTER_X86_FLAGS,
+    REGISTER_X86_MAX
 } reg_t;
 
 enum FLAGS
